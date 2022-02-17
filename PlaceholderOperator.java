484a112e1f7c496abf6458952bb42e54df070a08
BrandonWatanabeStudentAccount <99722500+BrandonWatanabeStudentAccount@users.noreply.github.com>
2022-02-16 16:50:09 -0800

    Added PlaceholderOperator
     so it doesn't crash when checking the stack

---
+++ PlaceholderOperator.java
@@ -0,0 +1,14 @@
+public class PlaceholderOperator extends Operator
+{
+    @Override
+    public int priority()
+    {
+        return 0;
+    }
+
+    @Override
+    public Operand execute(Operand operand1, Operand operand2)
+    {
+        return null;
+    }
+}
