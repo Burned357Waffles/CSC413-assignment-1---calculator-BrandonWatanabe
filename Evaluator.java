6aef95130a07d20d648531eb4dc347b024b7217a
BrandonWatanabeStudentAccount <99722500+BrandonWatanabeStudentAccount@users.noreply.github.com>
2022-02-16 17:53:11 -0800

    Got eval working and implemented UI

--- Evaluator.java
+++ Evaluator.java
@@ -27,34 +27,26 @@ public class Evaluator
 
     public void performOperation()
     {
-        // note that when we eval the expression 1 - 2 we will
-        // push the 1 then the 2 and then do the subtraction operation
-        // This means that the first number to be popped is the
-        // second operand, not the first operand - see the following code
-
         Operator oldOperator = operatorStack.pop();
         Operand operand2 = operandStack.pop();
         Operand operand1 = operandStack.pop();
         operandStack.push(oldOperator.execute(operand1, operand2));
     }
 
-    public boolean processParentheses(Operator newOperator)
+    public void processNewOperator(Operator newOperator)
     {
         if (newOperator == Operator.operatorHashMap.get("(")) {
             operatorStack.push(newOperator);
-            return false;
         } else if (newOperator == Operator.operatorHashMap.get(")")) {
             while (operatorStack.peek() != Operator.operatorHashMap.get("(")) {
                 performOperation();
             }
             operatorStack.pop();
-            return false;
         } else {
             while (operatorStack.peek().priority() >= newOperator.priority()) {
                 performOperation();
             }
             operatorStack.push(newOperator);
-            return true;
         }
     }
 
@@ -62,8 +54,6 @@ public class Evaluator
     {
         String token;
 
-        // The 3rd argument is true to indicate that the delimiters should be used
-        // as tokens, too. But, we'll need to remember to filter out spaces.
         this.tokenizer = new StringTokenizer(expression, DELIMITERS, true);
 
         if (operatorStack.size() == 0) {
@@ -71,51 +61,24 @@ public class Evaluator
         }
 
         while (this.tokenizer.hasMoreTokens()) {
-            // filter out spaces
             if (!(token = this.tokenizer.nextToken()).equals(" ")) {
-                // check if token is an operand
-                if (Operand.check(token)) { // if it is an operand
+                if (Operand.check(token)) {
                     operandStack.push(new Operand(token));
-                } else { // if it is not an operand
-                    if (!Operator.check(token)) { // if it is not a valid token STOP
+                } else {
+                    if (!Operator.check(token)) {
                         System.out.println("*****invalid token******");
                         System.exit(1);
                     }
 
-                    // TODO Operator is abstract - this line will need to be fixed:
-                    // ( The Operator class should contain an instance of a HashMap,
-                    // and values will be instances of the Operators.  See Operator class
-                    // skeleton for an example.
-
                     Operator newOperator = Operator.operatorHashMap.get(token);
-
-
-                    if (processParentheses(newOperator)) {
-                        continue;
-                    }
-
-                    if (operandStack.size() >= 2) {
-
-                        performOperation();
-
-
-                    }
+                    processNewOperator(newOperator);
                 }
             }
         }
-
-        // Control gets here when we've picked up all of the tokens; you must add
-        // code to complete the evaluation - consider how the code given here
-        // will evaluate the expression 1+2*3
-        // When we have no more tokens to scan, the operand stack will contain 1 2
-        // and the operator stack will have + * with 2 and * on the top;
-        // In order to complete the evaluation we must empty the stacks (except
-        // the init operator on the operator stack); that is, we should keep
-        // evaluating the operator stack until empty
-        // Suggestion: create a method that takes an operator as argument and
-        // then executes the while loop; also, move the stacks out of the main
-        // method
-
+        while (operatorStack.size() > 1) {
+            performOperation();
+        }
+        
         return operandStack.pop().getValue();
     }
 }
--- EvaluatorUI.java
+++ EvaluatorUI.java
@@ -3,7 +3,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class EvaluatorUI extends JFrame implements ActionListener {
+public class EvaluatorUI extends JFrame implements ActionListener
+{
   private TextField txField = new TextField();
   private Panel buttonPanel = new Panel();
 
@@ -11,47 +12,62 @@ public class EvaluatorUI extends JFrame implements ActionListener {
   // numbered from left to right, top to bottom
   // bText[] array contains the text for corresponding buttons
   private static final String[] bText = {
-    "7", "8", "9", "+", "4", "5", "6", "- ", "1", "2", "3",
-    "*", "0", "^", "=", "/", "(", ")", "C", "CE"
+          "7", "8", "9", "+", "4", "5", "6", "- ", "1", "2", "3",
+          "*", "0", "^", "=", "/", "(", ")", "C", "CE"
   };
-  private Button[] buttons = new Button[ bText.length ];
+  private Button[] buttons = new Button[bText.length];
 
-  public static void main(String argv[]) {
+  public static void main(String argv[])
+  {
     EvaluatorUI calc = new EvaluatorUI();
   }
 
-  public EvaluatorUI() {
-    setLayout( new BorderLayout() );
+  public EvaluatorUI()
+  {
+    setLayout(new BorderLayout());
 
-    add( txField, BorderLayout.NORTH );
-    txField.setEditable( false );
+    add(txField, BorderLayout.NORTH);
+    txField.setEditable(false);
 
-    add( buttonPanel, BorderLayout.CENTER );
-    buttonPanel.setLayout( new GridLayout( 5, 4 ));
+    add(buttonPanel, BorderLayout.CENTER);
+    buttonPanel.setLayout(new GridLayout(5, 4));
 
     //create 20 buttons with corresponding text in bText[] array
-    for ( int i = 0; i < 20; i++ ) {
-      buttons[ i ] = new Button( bText[ i ]);
+    for (int i = 0; i < 20; i++) {
+      buttons[i] = new Button(bText[i]);
     }
 
     //add buttons to button panel
-    for (int i=0; i<20; i++) {
-      buttonPanel.add( buttons[ i ]);
+    for (int i = 0; i < 20; i++) {
+      buttonPanel.add(buttons[i]);
     }
 
     //set up buttons to listen for mouse input
-    for ( int i = 0; i < 20; i++ ) {
-      buttons[ i ].addActionListener( this );
+    for (int i = 0; i < 20; i++) {
+      buttons[i].addActionListener(this);
     }
 
-    setTitle( "Calculator" );
-    setSize( 400, 400 );
-    setLocationByPlatform( true  );
-    setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
-    setVisible( true );
+    setTitle("Calculator");
+    setSize(400, 400);
+    setLocationByPlatform(true);
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    setVisible(true);
+
+    //init Evaluator
+
   }
 
-  public void actionPerformed( ActionEvent arg0 ) {
-    // You need to fill in this fuction
+  public void actionPerformed(ActionEvent arg0)
+  {
+    Evaluator evaluator = new Evaluator();
+
+    if (arg0.getSource().equals(buttons[14])) {
+      txField.setText(Integer.toString(evaluator.eval(txField.getText())));
+    } else if (arg0.getSource().equals(buttons[18]) || arg0.getSource().equals(buttons[19])) {
+      txField.setText("");
+    } else {
+      Button button = (Button) arg0.getSource();
+      txField.setText(txField.getText() + button.getLabel());
+    }
   }
 }
