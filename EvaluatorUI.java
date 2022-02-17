9d17e78036fa2b2af783708422fc188957cd3636
BrandonWatanabeStudentAccount <99722500+BrandonWatanabeStudentAccount@users.noreply.github.com>
2022-02-16 17:55:49 -0800

    Removed comments

--- EvaluatorUI.java
+++ EvaluatorUI.java
@@ -8,9 +8,6 @@ public class EvaluatorUI extends JFrame implements ActionListener
   private TextField txField = new TextField();
   private Panel buttonPanel = new Panel();
 
-  // total of 20 buttons on the calculator,
-  // numbered from left to right, top to bottom
-  // bText[] array contains the text for corresponding buttons
   private static final String[] bText = {
           "7", "8", "9", "+", "4", "5", "6", "- ", "1", "2", "3",
           "*", "0", "^", "=", "/", "(", ")", "C", "CE"
@@ -32,17 +29,14 @@ public class EvaluatorUI extends JFrame implements ActionListener
     add(buttonPanel, BorderLayout.CENTER);
     buttonPanel.setLayout(new GridLayout(5, 4));
 
-    //create 20 buttons with corresponding text in bText[] array
     for (int i = 0; i < 20; i++) {
       buttons[i] = new Button(bText[i]);
     }
 
-    //add buttons to button panel
     for (int i = 0; i < 20; i++) {
       buttonPanel.add(buttons[i]);
     }
 
-    //set up buttons to listen for mouse input
     for (int i = 0; i < 20; i++) {
       buttons[i].addActionListener(this);
     }
@@ -52,9 +46,6 @@ public class EvaluatorUI extends JFrame implements ActionListener
     setLocationByPlatform(true);
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     setVisible(true);
-
-    //init Evaluator
-
   }
 
   public void actionPerformed(ActionEvent arg0)
